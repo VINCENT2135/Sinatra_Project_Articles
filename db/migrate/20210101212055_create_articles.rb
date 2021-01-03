aea04c9dbbce0b41cacc55ba2b09594986c48284
VINCENT2135 <68698502+VINCENT2135@users.noreply.github.com>
2021-01-03 14:30:33 -0500

    DB Upload/Table Migration

---
+++ db/migrate/20210101212055_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+    t.string :title
+    t.text :method
+    t.integer :user_id
+    end 
+  end
+end
