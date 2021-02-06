dc497eb46b9eff7789c3e43aac2bf5fb52c9e0cc
VINCENT2135 <68698502+VINCENT2135@users.noreply.github.com>
2021-02-06 11:36:16 -0500

    Index & New Routes

--- articles/app/controllers/articles_controller.rb
+++ articles/app/controllers/articles_controller.rb
@@ -1,4 +1,16 @@
 class ArticlesController < ApplicationController
+      
+      get "/articles" do
+      @articles=Article.all
+      erb :'articles/index'
+    end
+
+
+   
+    get '/articles/new' do 
+      erb :'/articles/new' 
+  
+    end 
 
 end
       
