b09053253557921de2cbec6b9c693aa2dfe64709
Vincent Ivos <vincentivos1@gmail.com>
2021-02-07 13:02:59 -0500

    Validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,15 @@ class ApplicationController < Sinatra::Base
 
 
 
+  def current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+
 
 
 
