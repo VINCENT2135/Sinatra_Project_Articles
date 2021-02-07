572ff66dc06bdc8fa0adeaf038ec6b985a351702
Vincent Ivos <vincentivos1@gmail.com>
2021-02-06 21:36:37 -0500

    User Routes

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,28 @@
-class  UserController < ApplicationController
+class UsersController <  ApplicationController 
+    
+   
+    #index
+    get "/users" do
+        @users=User.all
+        erb :'users/index'
+    end
+
+    get '/login' do
+        erb :'users/login'
+     end
+
+     get '/signup' do
+        erb :'users/signup'
+    end
+    
 
 
+     
+    get "/users/:id/edit" do
+   
+        erb :'users/edit'
 
-end 
+        # redirect '/articles'
+     
+        end
+    end
--- config.ru
+++ config.ru
@@ -4,7 +4,7 @@ if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
-use UserController 
+use UsersController 
 use ArticleController
 run ApplicationController
 
